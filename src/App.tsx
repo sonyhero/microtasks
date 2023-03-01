9d1a449d00f44880d2509b201d296d43c2e679f9
sonyhero <27anton06@gmail.com>
2023-03-01 18:37:12 +0300

    microtasks-button

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,29 @@ import './App.css';
 // import {Header} from "./site/Header";
 // import {Body} from "./site/Body";
 // import {Footer} from "./site/Footer";
-import {NewComponent} from "./map/NewComponent";
+// import {NewComponent} from "./map/NewComponent";
+
+/*const students = [
+    {id: 1, name: "James", age: 8},
+    {id: 2, name: "Robert", age: 18},
+    {id: 3, name: "John", age: 28},
+    {id: 4, name: "Michael", age: 38},
+    {id: 5, name: "William", age: 48},
+    {id: 6, name: "David", age: 58},
+    {id: 7, name: "Richard", age: 68},
+    {id: 8, name: "Joseph", age: 78},
+    {id: 9, name: "Thomas", age: 88},
+    {id: 10, name: "Charles", age: 98},
+    {id: 11, name: "Christopher", age: 100},
+]*/
 
 function App() {
-    const students = [
-        {id: 1, name: "James", age: 8},
-        {id: 2, name: "Robert", age: 18},
-        {id: 3, name: "John", age: 28},
-        {id: 4, name: "Michael", age: 38},
-        {id: 5, name: "William", age: 48},
-        {id: 6, name: "David", age: 58},
-        {id: 7, name: "Richard", age: 68},
-        {id: 8, name: "Joseph", age: 78},
-        {id: 9, name: "Thomas", age: 88},
-        {id: 10, name: "Charles", age: 98},
-        {id: 11, name: "Christopher", age: 100},
-    ]
+
     return (
-        <>
-            <NewComponent students={students}/>
-        </>
+        <div className='App'>
+            <button>MyYouTubeChanel-1</button>
+        </div>
+
     );
 }
 
