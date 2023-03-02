8a166d610e3b8c5c7de2cb2b5f22bf240fbdd930
sonyhero <27anton06@gmail.com>
2023-03-02 15:48:21 +0300

    microtasks-refactoring-useState

---
+++ src/AppFolder/AppUseState.tsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+function AppUseState() {
+    return (
+        <div className='App'>
+            <button>MyYouTubeChanel-1</button>
+        </div>
+    )
+}
+
+export default AppUseState
