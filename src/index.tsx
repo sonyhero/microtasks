1b5b1a5dc292b84655a5fbb275bb91a7113c1651
sonyhero <27anton06@gmail.com>
2023-03-08 00:36:28 +0300

    add-changes-to-app.css

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
-import AppSite from "./AppFolder/AppSite";
-import AppStudents from "./AppFolder/AppStudents";
-import AppCars from "./AppFolder/AppCars";
-import AppButton from "./AppFolder/AppButton";
-import AppNewButton from "./AppFolder/AppNewButton";
-import AppUseState from "./AppFolder/AppUseState";
-import AppFilter from "./AppFolder/AppFilter";
+// import AppSite from "./AppFolder/AppSite";
+// import AppStudents from "./AppFolder/AppStudents";
+// import AppCars from "./AppFolder/AppCars";
+// import AppButton from "./AppFolder/AppButton";
+// import AppNewButton from "./AppFolder/AppNewButton";
+// import AppUseState from "./AppFolder/AppUseState";
+// import AppFilter from "./AppFolder/AppFilter";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <AppCars/>
+        <App />
     </React.StrictMode>
 );
 
