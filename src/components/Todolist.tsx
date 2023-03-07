13270f57557dc463f30f0d36516beaca1a1e5255
sonyhero <27anton06@gmail.com>
2023-03-07 22:47:14 +0300

    add-Todolist.tsx-to-training-map-and-filter-method

---
+++ src/components/Todolist.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+type TodolistType = {
+    title: string
+    tasks: Array<TasksType>
+}
+
+type TasksType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export function Todolist(props: TodolistType) {
+
+    const list = props.tasks.map(el => {
+        return (
+            <li key={el.id}><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
+        )
+    })
+
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input/>
+            <button>+</button>
+        </div>
+        <ul>
+            {list}
+        </ul>
+        <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+        </div>
+    </div>
+}
